7f2006d527f39d0a27e1554e21c4b52b03b43031
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-05 23:28:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
 // Write your code here
-let multiply,num1=31,num2=2;
+let multiply,num1=31,num2=2,num3=24,num4=5;
 multiply=num1*num2;
+let x=random();
+let remainder=num3%num4;
+let max=Math.max(1,20,12,19.5);
